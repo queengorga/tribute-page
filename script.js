3306fc8cab00f0d87115d2a7f760c53a4a731a34
Victoria Allen <vickmae.allen@gmail.com>
2018-08-17 19:40:24 -0700

    Added career section

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log()
+function bandDetails(evt, bandName) {
+
+    var i, content, links;
+
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+
+    links = document.getElementsByClassName("links");
+    for (i = 0; i < links.length; i++) {
+        links[i].className = links[i].className.replace(" active", "");
+    }
+
+    document.getElementById(bandName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
